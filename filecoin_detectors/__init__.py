4cfafcfa882f254a564ba05dee639ee149f6dc8e
nikitaka7 <nikitaka7@mail.ru>
2023-06-18 15:56:14 +0700

    fixed installation so it runs

--- filecoin_detectors/__init__.py
+++ filecoin_detectors/__init__.py
@@ -1,13 +1,10 @@
-from typing import Tuple, List, Type
-
-from slither_my_plugin.detectors.example import Example
-
-from slither.detectors.abstract_detector import AbstractDetector
-from slither.printers.abstract_printer import AbstractPrinter
+from filecoin_detectors.detectors.direct_precompile import DirectPrecompile
 
 
-def make_plugin() -> Tuple[List[Type[AbstractDetector]], List[Type[AbstractPrinter]]]:
-    plugin_detectors = [Example]
-    plugin_printers: List[Type[AbstractPrinter]] = []
+def make_plugin():
+    plugin_detectors = [
+        DirectPrecompile,
+    ]
+    plugin_printers = []
 
     return plugin_detectors, plugin_printers
--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     version="0.1",
     packages=find_packages(),
     python_requires=">=3.8",
-    install_requires=["slither-analyzer==0.1"],
+    install_requires=["slither-analyzer>=0.1"],
     entry_points={
-        "slither_analyzer.plugin": "slither my-plugin=slither_my_plugin:make_plugin",
+        "slither_analyzer.plugin": "slither my-plugin=filecoin_detectors:make_plugin",
     },
 )
