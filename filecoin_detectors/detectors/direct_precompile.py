9f2d759a7f93fcec8c1fc494dbbed349e5544adb
nikitaka7 <nikitaka7@mail.ru>
2023-06-18 17:38:28 +0700

    detector seems to work as it should

--- filecoin_detectors/detectors/direct_precompile.py
+++ filecoin_detectors/detectors/direct_precompile.py
@@ -2,7 +2,6 @@ from typing import List
 from slither.utils.output import Output
 from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
 from slither.core.declarations import Function
-#from slither.slithir.operations import LibraryCall
 
 
 class DirectPrecompile(AbstractDetector):
@@ -22,26 +21,30 @@ class DirectPrecompile(AbstractDetector):
     WIKI_RECOMMENDATION = 'Use precompiles through filecoin library'
 
 
-    def _has_direct_precompile (self, fun: Function) -> bool:   #если есть low level static call и используется в правой части адрес прекомпайла - это вхождение
-        """Checks if a function calls precompile directly"""
+    def _has_direct_precompile (self, fun: Function) -> bool: # function checks if there are direct precompile calls in the code
+        precompile_address_1 = '0xFE00000000000000000000000000000000000001'
+        precompile_address_2 = '0xfE00000000000000000000000000000000000002'
         if isinstance(fun, Function):   # check for a correct function type
-                if fun.low_level_calls:
-                    if 'staticcall' in str(fun.low_level_calls[0]):
-                        for node in fun.nodes:
-                            if '0xFE00000000000000000000000000000000000001' in str(node) or '0xfE00000000000000000000000000000000000002' in str(node):
-                                return True
-        return False
+            for node in fun.nodes:
+                if 'staticcall' in str(node): # precompiles are called with staticcalls
+                    for var in node.variables_read:
+                        if precompile_address_1 in str(var.expression) or precompile_address_2 in str(var.expression):
+                            return True
+                    for node in fun.nodes:
+                        if precompile_address_1 in str(node) or precompile_address_2 in str(node):
+                            return True
 
 
     def _detect(self) -> List[Output]:
         """Main function"""
         res = []
         for contract in self.compilation_unit.contracts_derived:
-            for f in contract.functions_and_modifiers_declared:
-                x = self._has_direct_precompile(f)
-                if x:
-                    res.append(self.generate_result([
-                        "Direct precompile call in ",
-                        f,
-                        '\n']))
+            if 'PrecompilesAPI' not in contract.name:
+                for f in contract.functions_and_modifiers_declared:
+                    x = self._has_direct_precompile(f)
+                    if x:
+                        res.append(self.generate_result([
+                            "Direct precompile call in ",
+                            f,
+                            '\n']))
         return res
